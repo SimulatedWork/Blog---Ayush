713375185fb6a6ed41deb9db1697a8999ac35d24
Ayush Shrivastav <ayush41red@gmail.com>
2023-10-20 14:12:48 +0530

    Added Navigation Through Pages and Components.

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,68 +1,108 @@
 import { useState } from "react";
 import "../css/Navbar.css";
 import { RotatingLines } from "react-loader-spinner";
+import Blogs from "../pages/Blogs";
+import About from "../pages/About";
+import UploadBlog from "../pages/UploadBlog";
+import Profile from "../pages/Profile";
 import { BsSearch } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 
 const Navbar = () => {
   const [loginState, setLoginState] = useState(false);
-  const handleElementChange = (e) => {
+  const [activeComponent, setActiveComponent] = useState(<Blogs />);
+  const handleElementChange = (e, componentName) => {
     const navComponents = document.querySelectorAll(".nav-comp");
     navComponents.forEach((el) => {
       el.classList.remove("active-space");
     });
+    console.log(componentName);
+    switch (componentName) {
+      case "blogs":
+        setActiveComponent(<Blogs />);
+        break;
+      case "about":
+        setActiveComponent(<About />);
+        break;
+      case "profile":
+        setActiveComponent(<Profile />);
+        break;
+      case "post":
+        setActiveComponent(<UploadBlog />);
+        break;
+      default:
+        break;
+    }
     e.target.classList.add("active-space");
   };
   const handleLoginButtonClick = () => {
     setLoginState(true);
   };
   return (
-    <div className="navbar">
-      <span>Bloggery</span>
-      <span className="search-container">
-        <input
-          type="text"
-          name="search"
-          className="search-bar"
-          placeholder="Search for title, category..."
-        />
-        <button className="icon button">
-          <img src="/search.svg" alt="search" height={24} />
-        </button>
-      </span>
-
-      <nav>
-        <span
-          className="active-space nav-comp"
-          onClick={(e) => handleElementChange(e)}
-        >
-          Blogs
-        </span>
-        <span className="nav-comp" onClick={(e) => handleElementChange(e)}>
-          About?
-        </span>
-        <span className="nav-comp" onClick={(e) => handleElementChange(e)}>
-          Profile
-        </span>
-        <span className="nav-comp" onClick={(e) => handleElementChange(e)}>
-          Post Blog
-        </span>
-      </nav>
-
-      <button className="login button" onClick={handleLoginButtonClick}>
-        {loginState ? (
-          <RotatingLines
-            strokeColor="black"
-            strokeWidth="4"
-            animationDuration="1.00"
-            width="32"
-            visible={true}
+    <>
+      <div className="navbar">
+        <span>Bloggery</span>
+        <span className="search-container">
+          <input
+            type="text"
+            name="search"
+            className="search-bar"
+            placeholder="Search for title, category..."
           />
-        ) : (
-          "Login"
-        )}
-      </button>
-    </div>
+          <button className="icon button">
+            <img src="/search.svg" alt="search" height={24} />
+          </button>
+        </span>
+
+        <nav>
+          <span
+            className="active-space nav-comp"
+            onClick={(e) => handleElementChange(e, "blogs")}
+          >
+            Blogs
+          </span>
+          <span
+            className="nav-comp"
+            name="about"
+            onClick={(e) => handleElementChange(e, "about")}
+          >
+            About?
+          </span>
+          <span
+            name="profile"
+            className="nav-comp"
+            onClick={(e) => handleElementChange(e, "profile")}
+          >
+            Profile
+          </span>
+          <span
+            name="post-blog"
+            className="nav-comp"
+            onClick={(e) => handleElementChange(e, "post")}
+          >
+            Post Blog
+          </span>
+        </nav>
+
+        <button className="login button" onClick={handleLoginButtonClick}>
+          {loginState ? (
+            <RotatingLines
+              strokeColor="black"
+              strokeWidth="4"
+              animationDuration="1.00"
+              width="32"
+              visible={true}
+            />
+          ) : (
+            "Login"
+          )}
+        </button>
+        <div className={`loginModal ${loginState ? "show" : ""} `}>
+          <div className="modalContent"></div>
+        </div>
+      </div>
+      {activeComponent}
+    </>
   );
 };
 
---
+++ client/src/pages/Blogs.jsx
@@ -0,0 +1,6 @@
+
+const Blogs = () => {
+  return <div>This is Blogs</div>;
+};
+
+export default Blogs;
