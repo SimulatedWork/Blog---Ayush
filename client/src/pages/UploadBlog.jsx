2e85f41331c5d7d799b5efb0f3f132c0fb0f4af2
Ayush Shrivastav <ayush41red@gmail.com>
2023-10-21 16:47:47 +0530

    Made the basic Post blog page

--- client/src/pages/UploadBlog.jsx
+++ client/src/pages/UploadBlog.jsx
@@ -1,6 +1,33 @@
+import { AiFillFolder } from "react-icons/ai";
+import "../css/UploadBlogs.css";
 
 const UploadBlog = () => {
-  return <div>This is upload</div>;
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }} className="BlogPostingHead">
+        {" "}
+        <span>Create</span> And <span>Post</span> A New Blog:
+      </h1>
+      <div className="BlogPostingParentContainer">
+        <div className="BlogPostingInputContainer">
+          <input
+            type="text"
+            className="BlogTitle"
+            placeholder="Title for your blog post:"
+          />
+          <div className="BlogCoverImage">
+            <p>Choose a cover image for your blog</p>
+            <AiFillFolder />
+          </div>
+          <input type="file" style={{ display: "none" }} />
+          <button className="BlogPostingPublishButton">Publish</button>
+        </div>
+        <div className="BlogPostingContentContainer">
+          <textarea name="content" className="blogContent" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UploadBlog;
