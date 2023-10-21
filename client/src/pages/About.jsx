aeaa2cacc916313c9700162b634e250b7d7aab50
Ayush Shrivastav <ayush41red@gmail.com>
2023-10-21 14:55:56 +0530

    Add About Component

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,23 @@
+import "../css/About.css";
 
 const About = () => {
-  return <div>This is about</div>;
+  return (
+    <div className="content-container">
+      <h1>
+        Get the most <br />
+        <span>Curated Knowledge </span> <br />
+        from Around the <br />
+        World.
+      </h1>
+      <h1>
+        <span>Discover, Post, Create</span>: <br />
+        Your Gateway to{" "}
+        <span>
+          Global <br /> Insights.
+        </span>
+      </h1>
+    </div>
+  );
 };
 
 export default About;
