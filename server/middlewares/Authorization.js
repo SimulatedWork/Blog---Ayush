a5ddb2a7aa7db7385f3b717fade48be913772f39
Ayush Shrivastav <ayush41red@gmail.com>
2023-11-08 17:02:59 +0530

    Changed the authorization file.

--- server/middlewares/Authorization.js
+++ server/middlewares/Authorization.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const authenticateToken = (req, resp, next) => {
   const authHeader = req.headers["authorization"];
   if (!authHeader) {
-    return resp.status(401).json({ error: "Authorization Header is required" });
+    resp.status(401).json({ error: "Authorization Header is required" });
   }
 
   const token = authHeader.split(" ")[1];
@@ -13,7 +13,7 @@ const authenticateToken = (req, resp, next) => {
     req.user = decodedToken;
     next();
   } catch (e) {
-    return resp.status(401).json({ error: "Invalid Token." });
+    resp.status(401).json({ error: "Invalid Token." });
   }
 };
 
